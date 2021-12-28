bb29794ce080323b7328a6214b35586e9580cc29
Xeonphobia <XeonPhobia@users.noreply.github.com>
2021-12-28 20:19:06 +0100

    testing map()

---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+test_variable = np.array([[[ 81,  23,  22], [255, 201, 200]] , [[124,  66 , 65], [155,  97,  96]]])
+
+print(test_variable[0])
