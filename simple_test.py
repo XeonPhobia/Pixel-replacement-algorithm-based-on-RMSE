2326ef97706eaa6e7bdcce9cb7d8b4806d8f1363
Xeonphobia <XeonPhobia@users.noreply.github.com>
2022-02-01 13:17:33 +0100

    simple_test.py is working

--- simple_test.py
+++ simple_test.py
@@ -1,29 +1,10 @@
-from pickletools import uint8
-#from numba import jit
+from numba import jit
 import numba
 import numpy as np
-import cupy as cp
 import math
 
-# @jit
-# def add1(x):
-#     return x + 1
-
-# @jit
-# def add1array(input):
-#     for key in range(len(input)):
-#         input[key] = 3
-#     return input
-
-# input_array = np.array([[81, 23, 22], [255, 201, 200], [124, 66, 65], [155, 97, 96]], dtype=np.uint8)
-# input_list = numba.typed.List()
-# input_list.append(input_array)
-# print(input_list[0][0])
-# input_list[0][0].pop()
-#AttributeError: 'numpy.ndarray' object has no attribute 'pop'
-
+@jit#(nopython=True)
 def calculate_RMSE(input_array, comparison_array):
-    #input_array = np.array(input_tensor)   
     diff_array1 = (int(input_array[2]) - int(comparison_array[0]))**2 
     diff_array2 = (int(input_array[3]) - int(comparison_array[1]))**2 
     diff_array3 = (int(input_array[4]) - int(comparison_array[2]))**2      
@@ -31,41 +12,40 @@ def calculate_RMSE(input_array, comparison_array):
     diff_array = math.sqrt(diff_array)
     return diff_array
 
-# def replace_pixel(input_tensor, test_variable):
-#     output_array = np.zeros(shape=(len(test_variable),1))
-#     for key, key_value in enumerate(test_variable):
-#         output_array[key] = calculate_RMSE(input_tensor, key_value)
-#     result = np.where(output_array == np.amin(output_array))
-#     return test_variable.pop(result[0][0])
+@jit
+def replace_pixel(input_list, filter_1D_array):
+    output_array = np.zeros(shape=(len(input_list),1))
+    for key, key_value in enumerate(input_list):
+        output_array[key] = calculate_RMSE(key_value, filter_1D_array)
+    result = np.where(output_array == np.amin(output_array))
+    return result[0][0]
 
-input_array = np.array([0, 0, 81, 23, 22], dtype=np.uint8)
-input_array2 = np.array([0, 1, 255, 201, 200], dtype=np.uint8)
-input_array3 = np.array([1, 0, 124, 66, 65], dtype=np.uint8)
-filter_array = np.array([155, 97, 96], dtype=np.uint8)
-input_list = numba.typed.List()
-input_list.append(input_array)
-input_list.append(input_array2)
-input_list.append(input_array3)
+if __name__=='__main__':
+    # source_list(x_direction, y_direction, R, G, B)
+    input_array = np.array([[81, 23, 22],[255, 201, 200],[124, 66, 65]], dtype=np.uint8)
+    input_list = numba.typed.List()
 
 
-print(calculate_RMSE(input_list[0], filter_array))
+    filter_array = np.array([155, 97, 96], dtype=np.uint8)
+    filter_list = numba.typed.List()
+    
+    for key, key_value in enumerate(input_array):
+        print(f"key:{key} value: {key_value}")
+        input_list.append([0, key, key_value[0], key_value[1], key_value[2]], dtype=int)
+    #     for key2, key_value2 in enumerate(key_value):
+    #         key_value[key2] = replace_pixel(source_image[key][key2], filter_image)
+    print(input_list)
 
 
 
+    
+    # result_array = np.zeros((3,1,3), dtype=np.uint8)
 
+    # key = 0
+    # output_value = replace_pixel(input_list, filter_list[key])
+    # result_array[input_list[output_value][0]][input_list[output_value][1]] = input_list[output_value][2:5]
+    # input_list.pop(output_value)
+    # filter_list.pop(key)
+    # #print(f"result_list is: {result_array}")
 
-
-
-# source_list(x_direction, y_direction, R, G, B)
-#source_list = numba.typed.List([0, 0, 255, 250, 240])
-#source_list = source_list.append([1, 0, 81, 23, 22])
-#filter_list(R, G, B, darkness(R + G + B))
-#filter_list = cp.array([[81, 23, 22], [255, 201, 200], [124, 66, 65], [155, 97, 96]], dtype=uint8)
-#print(source_list)
-
-
-#print(filter_list)
-
-#print(add1array(input_list))
-
-
+    
--- test.py
+++ test.py
@@ -1,33 +1,50 @@
-import tensorflow as tf
+from numba import jit
+import numba
 import numpy as np
-import matplotlib.pyplot as plt
 import math
-from tensorflow.python.keras.backend import variable
-import random
 
-def calculate_RMSE(input_tensor, comparison_array):
-    input_array = np.array(input_tensor[0])   
-    diff_array1 = (int(input_array[0]) - int(comparison_array[0]))**2 
-    diff_array2 = (int(input_array[1]) - int(comparison_array[1]))**2 
-    diff_array3 = (int(input_array[2]) - int(comparison_array[2]))**2      
+@jit#(nopython=True)
+def calculate_RMSE(input_array, comparison_array):
+    diff_array1 = (int(input_array[2]) - int(comparison_array[0]))**2 
+    diff_array2 = (int(input_array[3]) - int(comparison_array[1]))**2 
+    diff_array3 = (int(input_array[4]) - int(comparison_array[2]))**2      
     diff_array = diff_array1 + diff_array2 + diff_array3 
     diff_array = math.sqrt(diff_array)
     return diff_array
 
-def replace_pixel(input_tensor, test_variable):
-    output_array = np.zeros(shape=(len(test_variable),1))
-    for key in range(len(test_variable)):
-        output_array[key] = calculate_RMSE(input_tensor, test_variable[key])
+@jit
+def replace_pixel(input_list, filter_1D_array):
+    output_array = np.zeros(shape=(len(input_list),1))
+    for key, key_value in enumerate(input_list):
+        output_array[key] = calculate_RMSE(key_value, filter_1D_array)
     result = np.where(output_array == np.amin(output_array))
-    return test_variable.pop(result[0][0])
+    return result[0][0]
 
-input_tensor = tf.constant([[[100, 110, 120]]], dtype=np.uint8)
-test_variable = np.array([[[81, 23, 22], [255, 201, 200]], [[124, 66, 65], [155, 97, 96]]], dtype=np.uint8)
-test_variable = test_variable.reshape(len(test_variable) + len(test_variable[0]) ,3)
-test_variable = test_variable.tolist()
-result_Array = np.zeros((len(test_variable),len(test_variable[0]),3), dtype=np.uint8)
+if __name__=='__main__':
+    # source_list(x_direction, y_direction, R, G, B)
+    input_array = np.array([[81, 23, 22],[255, 201, 200],[124, 66, 65]], dtype=np.uint8)
+    input_list = numba.typed.List()
+    
+    for key, key_value in enumerate(input_array):
+        print(f"key:{key} value: {key_value}")
+        tmp_array = np.array([0, key, key_value[0], key_value[1], key_value[2]], dtype=int)
+        input_list.append(tmp_array)
+
+    #     for key2, key_value2 in enumerate(key_value):
+    #         key_value[key2] = replace_pixel(source_image[key][key2], filter_image)
+    print(input_list)
 
 
+    filter_array = np.array([155, 97, 96], dtype=np.uint8)
+    filter_list = numba.typed.List()
+    
+    # result_array = np.zeros((3,1,3), dtype=np.uint8)
 
-tmp2 = replace_pixel(input_tensor[0], test_variable)
-print(f"the result of replace pixel is: {tmp2}")
+    # key = 0
+    # output_value = replace_pixel(input_list, filter_list[key])
+    # result_array[input_list[output_value][0]][input_list[output_value][1]] = input_list[output_value][2:5]
+    # input_list.pop(output_value)
+    # filter_list.pop(key)
+    # #print(f"result_list is: {result_array}")
+
+    
