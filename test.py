32c8387c8ccc4b71887b47f60a3a1582711525d8
Xeonphobia <XeonPhobia@users.noreply.github.com>
2022-02-26 21:30:59 +0100

    update comments

--- test.py
+++ test.py
@@ -19,12 +19,15 @@ def calculate_RMSE():
 
 
 if __name__=='__main__':
+    #Convert two .jpg images to tensors with R G B values between 0 and 255:
     input_tensor = tf.constant([[[81, 23, 22],[255, 201, 200]],[[124, 66, 65],[0, 0, 0]]], dtype=np.uint8)
     filter_tensor = tf.Variable([[[35, 132, 10],[137, 184, 30]],[[66, 12, 254],[0, 0, 0]]], dtype=np.uint8)
+
+    #Convert tensors to integers. 
     input_tensor = tf.cast(input_tensor, dtype=np.int32)
-    total_Score = tf.zeros(1, dtype=np.float32)
-    
     filter_tensor = tf.cast(filter_tensor, dtype=np.int32)
+ 
+    #Heatmap of error. 
     rmse_difference = tf.zeros([input_tensor.shape[0], input_tensor.shape[1], 3], dtype=np.int32)
     
     total_Score = calculate_RMSE()
@@ -32,6 +35,5 @@ if __name__=='__main__':
     # I would like to write an optimizer in tensorflow that reorders [R, G, B] array in filter_tensor variable.
     # The goal is to get total_Score to be as small as possible. 
 
-    
 
     print(total_Score)
