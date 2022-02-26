6bbfe361650a942f217e90d50d814314c45d7c9c
Xeonphobia <XeonPhobia@users.noreply.github.com>
2022-02-26 21:22:30 +0100

    testing with tensors instead

--- test.py
+++ test.py
@@ -1,50 +1,37 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 from numba import jit
 import numba
 import numpy as np
 import math
+import tensorflow as tf
 
-@jit#(nopython=True)
-def calculate_RMSE(input_array, comparison_array):
-    diff_array1 = (int(input_array[2]) - int(comparison_array[0]))**2 
-    diff_array2 = (int(input_array[3]) - int(comparison_array[1]))**2 
-    diff_array3 = (int(input_array[4]) - int(comparison_array[2]))**2      
-    diff_array = diff_array1 + diff_array2 + diff_array3 
-    diff_array = math.sqrt(diff_array)
-    return diff_array
 
-@jit
-def replace_pixel(input_list, filter_1D_array):
-    output_array = np.zeros(shape=(len(input_list),1))
-    for key, key_value in enumerate(input_list):
-        output_array[key] = calculate_RMSE(key_value, filter_1D_array)
-    result = np.where(output_array == np.amin(output_array))
-    return result[0][0]
+def calculate_RMSE():
+    rmse_difference = tf.math.subtract(input_tensor, filter_tensor)
+    rmse_difference = tf.math.square(rmse_difference)
+    rmse_difference = tf.reduce_sum(rmse_difference, -1)
+    rmse_difference = tf.cast(rmse_difference, dtype=np.float32)
+    rmse_difference = tf.math.sqrt(rmse_difference)
+    return tf.reduce_sum(rmse_difference)
+
+
 
 if __name__=='__main__':
-    # source_list(x_direction, y_direction, R, G, B)
-    input_array = np.array([[81, 23, 22],[255, 201, 200],[124, 66, 65]], dtype=np.uint8)
-    input_list = numba.typed.List()
+    input_tensor = tf.constant([[[81, 23, 22],[255, 201, 200]],[[124, 66, 65],[0, 0, 0]]], dtype=np.uint8)
+    filter_tensor = tf.Variable([[[35, 132, 10],[137, 184, 30]],[[66, 12, 254],[0, 0, 0]]], dtype=np.uint8)
+    input_tensor = tf.cast(input_tensor, dtype=np.int32)
+    total_Score = tf.zeros(1, dtype=np.float32)
     
-    for key, key_value in enumerate(input_array):
-        print(f"key:{key} value: {key_value}")
-        tmp_array = np.array([0, key, key_value[0], key_value[1], key_value[2]], dtype=int)
-        input_list.append(tmp_array)
-
-    #     for key2, key_value2 in enumerate(key_value):
-    #         key_value[key2] = replace_pixel(source_image[key][key2], filter_image)
-    print(input_list)
-
-
-    filter_array = np.array([155, 97, 96], dtype=np.uint8)
-    filter_list = numba.typed.List()
+    filter_tensor = tf.cast(filter_tensor, dtype=np.int32)
+    rmse_difference = tf.zeros([input_tensor.shape[0], input_tensor.shape[1], 3], dtype=np.int32)
     
-    # result_array = np.zeros((3,1,3), dtype=np.uint8)
+    total_Score = calculate_RMSE()
 
-    # key = 0
-    # output_value = replace_pixel(input_list, filter_list[key])
-    # result_array[input_list[output_value][0]][input_list[output_value][1]] = input_list[output_value][2:5]
-    # input_list.pop(output_value)
-    # filter_list.pop(key)
-    # #print(f"result_list is: {result_array}")
+    # I would like to write an optimizer in tensorflow that reorders [R, G, B] array in filter_tensor variable.
+    # The goal is to get total_Score to be as small as possible. 
 
     
+
+    print(total_Score)
